26a466afaab74d9b6ffb298c07408f77dec7574e
Israel Adura <israeladura@gmail.com>
2017-10-23 18:44:28 +0100

    version 1.0

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports={
     mlab:'mongodb://israeladura:this.code@ds125565.mlab.com:25565/sparkplugnow',
     mongo:'mongodb://localhost/nodejslogin',
-    admin:'59edd9e545264e264993bb76'
-    //'59edd9e545264993bb76'-mlab
-    //59eb6495d8ba7a11169b4069-local
+    admin:'59eb1bfcd2575a4279cbe35b'
+   
 }
+ //'59eb1bfcd2575a4279cbe35b'-mlab
+    //59eb6495d8ba7a11169b4069-local
